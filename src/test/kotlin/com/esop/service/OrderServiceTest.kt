689d49e1012dd92bd03be04ec136266f059a88da
sankar <sankaranarayananm@sahaj.ai>
2023-01-30 13:28:06 +0530

    Add unit testcase

--- src/test/kotlin/com/esop/service/OrderServiceTest.kt
+++ src/test/kotlin/com/esop/service/OrderServiceTest.kt
@@ -157,4 +157,42 @@ class OrderServiceTest {
             userList["arun"]!!.orderList[userList["arun"]!!.orderList.indexOf(sellOrderByArun)].orderStatus
         )
     }
+
+    @Test
+    fun `It should place 2 BUY orders followed by a SELL order where the SELL order is partial`() {
+        //Arrange
+        userList["sankar"]!!.userWallet.addMoneyToWallet(100)
+        val buyOrderBySankar = Order(10, "BUY", 10, "sankar")
+        userList["sankar"]!!.userWallet.moveMoneyFromFreeToLockedState(100)
+        placeOrder(buyOrderBySankar)
+
+        userList["aditya"]!!.userWallet.addMoneyToWallet(100)
+        val buyOrderByAditya = Order(10, "BUY", 10, "aditya")
+        userList["sankar"]!!.userWallet.moveMoneyFromFreeToLockedState(100)
+        placeOrder(buyOrderByAditya)
+
+        userList["kajal"]!!.userNonPerfInventory.addESOPsToInventory(50)
+        val sellOrderByKajal = Order(25, "SELL", 10, "kajal")
+        userList["kajal"]!!.userNonPerfInventory.moveESOPsFromFreeToLockedState(25)
+
+        //Act
+        placeOrder(sellOrderByKajal)
+
+        //Assert
+        assertEquals(25, userList["kajal"]!!.userNonPerfInventory.getFreeInventory())
+        assertEquals(10, userList["sankar"]!!.userNonPerfInventory.getFreeInventory())
+        assertEquals(10, userList["aditya"]!!.userNonPerfInventory.getFreeInventory())
+        assertEquals(196, userList["kajal"]!!.userWallet.getFreeMoney())
+        assertEquals(0, userList["sankar"]!!.userWallet.getFreeMoney())
+        assertEquals(0, userList["sankar"]!!.userWallet.getFreeMoney())
+        assertEquals("PARTIAL", sellOrders[sellOrders.indexOf(sellOrderByKajal)]!!.orderStatus)
+        assertEquals(
+            "COMPLETED",
+            userList["sankar"]!!.orderList[userList["sankar"]!!.orderList.indexOf(buyOrderBySankar)].orderStatus
+        )
+        assertEquals(
+            "COMPLETED",
+            userList["aditya"]!!.orderList[userList["aditya"]!!.orderList.indexOf(buyOrderByAditya)].orderStatus
+        )
+    }
 }
