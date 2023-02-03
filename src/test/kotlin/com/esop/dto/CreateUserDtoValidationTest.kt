323b0be58a5609c3c9bfb3baf5f655696363350a
Arun Murugan <arunmurugan@sahaj.ai>
2023-02-03 14:22:28 +0530

    add positive test cases to validate first and last name

--- src/test/kotlin/com/esop/dto/CreateUserDtoValidationTest.kt
+++ src/test/kotlin/com/esop/dto/CreateUserDtoValidationTest.kt
@@ -21,6 +21,15 @@ class CreateUserDtoValidationTest {
         Assertions.assertTrue(errors.find { it.message == "First Name is required" } != null)
     }
 
+    @Test
+    fun `it should not generate error if first name is not null`() {
+        val userDetails = UserCreationDTO(firstName = "arun")
+
+        val errors = validator.validate(userDetails)
+
+        Assertions.assertTrue(errors.find { it.message == "First Name is required" } == null)
+    }
+
 
     @Test
     fun `it should generate error if last name is null`() {
@@ -29,6 +38,15 @@ class CreateUserDtoValidationTest {
         val errors = validator.validate(userDetails)
 
         Assertions.assertTrue(errors.find { it.message == "Last Name is required" } != null)
+    }
+
+    @Test
+    fun `it should not generate error if last name is not null`() {
+        val userDetails = UserCreationDTO(lastName = "murugan")
+
+        val errors = validator.validate(userDetails)
+
+        Assertions.assertTrue(errors.find { it.message == "Last Name is required" } == null)
     }
 
 
@@ -41,6 +59,16 @@ class CreateUserDtoValidationTest {
         Assertions.assertTrue(errors.find { it.message == "First Name should not contain consecutive whitespaces" } != null)
     }
 
+    @Test
+    fun `it should not generate error if first name doesn't contain two consecutive whitespaces`() {
+        val userDetails = UserCreationDTO(firstName = "Mahesh Aravind")
+
+        val errors = validator.validate(userDetails)
+
+        Assertions.assertTrue(errors.find { it.message == "First Name should not contain consecutive whitespaces" } == null)
+    }
+
+
 
     @Test
     fun `it should generate error if last name contains two consecutive whitespaces`() {
@@ -49,6 +77,15 @@ class CreateUserDtoValidationTest {
         val errors = validator.validate(userDetails)
 
         Assertions.assertTrue(errors.find { it.message == "Last Name should not contain consecutive whitespaces" } != null)
+    }
+
+    @Test
+    fun `it should generate not error if last name doesn't contain two consecutive whitespaces`() {
+        val userDetails = UserCreationDTO(lastName = "Arun Murugan")
+
+        val errors = validator.validate(userDetails)
+
+        Assertions.assertTrue(errors.find { it.message == "Last Name should not contain consecutive whitespaces" } == null)
     }
 
     @Test
@@ -61,12 +98,30 @@ class CreateUserDtoValidationTest {
     }
 
     @Test
+    fun `it should not generate error if first name does not contain digits`() {
+        val userDetails = UserCreationDTO(firstName = "arun")
+
+        val errors = validator.validate(userDetails)
+
+        Assertions.assertTrue(errors.find { it.message == "First Name should not contain number digits" } == null)
+    }
+
+    @Test
     fun `it should generate error if last name contains digits`() {
         val userDetails = UserCreationDTO(lastName = "a 1")
 
         val errors = validator.validate(userDetails)
 
         Assertions.assertTrue(errors.find { it.message == "Last Name should not contain number digits" } != null)
+    }
+
+    @Test
+    fun `it should not generate error if last name does not contain digits`() {
+        val userDetails = UserCreationDTO(lastName = "Mahesh")
+
+        val errors = validator.validate(userDetails)
+
+        Assertions.assertTrue(errors.find { it.message == "Last Name should not contain number digits" } == null)
     }
 
 
@@ -80,12 +135,31 @@ class CreateUserDtoValidationTest {
     }
 
     @Test
+    fun `it should not generate error if first name does not contain special characters`() {
+        val userDetails = UserCreationDTO(firstName = "arun")
+
+        val errors = validator.validate(userDetails)
+
+        Assertions.assertTrue(errors.find { it.message == "First Name should not contain special characters" } == null)
+    }
+
+
+    @Test
     fun `it should generate error if last name contains special characters`() {
         val userDetails = UserCreationDTO(lastName = "arun@")
 
         val errors = validator.validate(userDetails)
 
         Assertions.assertTrue(errors.find { it.message == "Last Name should not contain special characters" } != null)
+    }
+
+    @Test
+    fun `it should not generate error if last name does not contain special characters`() {
+        val userDetails = UserCreationDTO(lastName = "arun")
+
+        val errors = validator.validate(userDetails)
+
+        Assertions.assertTrue(errors.find { it.message == "Last Name should not contain special characters" } == null)
     }
 
 
@@ -99,4 +173,12 @@ class CreateUserDtoValidationTest {
         Assertions.assertTrue(errors.find { it.message == "combination of First Name and Last Name should not exceed 64 characters" } != null)
     }
 
+    @Test
+    fun `it should not generate error if length after combining first name and last name is lesser than or equal to 64 characters`() {
+        val userDetails = UserCreationDTO(firstName = "abcdefghij".repeat(6), lastName = "abc")
+
+        val errors = validator.validate(userDetails)
+
+        Assertions.assertTrue(errors.find { it.message == "combination of First Name and Last Name should not exceed 64 characters" } == null)
+    }
 }
