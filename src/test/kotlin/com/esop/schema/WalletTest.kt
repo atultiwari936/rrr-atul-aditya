339a4d47bb7ebbc184be217d3550d785647d0142
Aditya Kedar Tiwari <aditakedart@Adityas-MacBook-Pro.local>
2023-02-08 13:37:48 +0530

    PR Changes: negative test cases for function moveMoneyFromLockedToFree & moveMoneyFromLockedToFree

--- src/test/kotlin/com/esop/schema/WalletTest.kt
+++ src/test/kotlin/com/esop/schema/WalletTest.kt
@@ -21,4 +21,30 @@ class WalletTest {
         assertEquals(expectedLockedAmount,wallet.getLockedMoney())
     }
 
+    @Test
+    fun `it should move money from locked to free state for negative amount given`(){
+        val wallet = Wallet()
+        wallet.addMoneyToWallet(100)
+        wallet.moveMoneyFromFreeToLockedState(50)
+
+        wallet.moveMoneyFromLockedToFree(-25)
+
+        val expectedFreeAmount = 25L
+        val expectedLockedAmount = 75L
+
+        assertEquals(expectedFreeAmount, wallet.getFreeMoney())
+        assertEquals(expectedLockedAmount,wallet.getLockedMoney())
+    }
+
+    @Test
+    fun `it should check for insufficient funds`(){
+        val wallet = Wallet()
+        wallet.addMoneyToWallet(100)
+
+        val response = wallet.moveMoneyFromFreeToLockedState(150)
+
+        val expectedResult = "Insufficient funds"
+        assertEquals(expectedResult,response)
+    }
+
 }
